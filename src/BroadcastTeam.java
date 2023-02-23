f855bdb7ad91adedbf2e1eac9bc8441c6af3ccfe
cbs970524 <58094664+cbs970524@users.noreply.github.com>
2023-02-23 18:03:35 +0900

    0223 18:03 commit

--- src/BroadcastTeam.java
+++ src/BroadcastTeam.java
@@ -1,5 +1,47 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class BroadcastTeam {
-    int hi;
-    int why;
-    String a="드디어 되나";
+    ArrayList<String> team;
+    ArrayList<String> member;
+    ArrayList<String> camera;
+    ArrayList<String> speaker;
+    ArrayList<String> mic;
+    ArrayList<String> computer;
+
+    void BroadcastTeam(){
+        this.team = new ArrayList<>();
+        this.member = new ArrayList<>();
+        this.camera = new ArrayList<>();
+        this.speaker = new ArrayList<>();
+        this.mic = new ArrayList<>();
+        this.computer = new ArrayList<>();
+    }
+    void broadcasting(String type) {
+        System.out.println(type + "방송을 시작합니다");
+    }
+    void addNewMember(String member){
+        this.member.add(member);
+    }
+    void deleteMember(String member){
+        this.member.remove(member);
+    }
+    void buyCamera(String camera){
+        this.camera.add(camera);
+    }
+    void fixCamera(String camera){
+        int find = this.camera.indexOf(camera);
+        if (find == -1){
+            System.out.println("보유하지 않은 장비입니다.");
+        }
+        else{
+            this.camera.remove(find);
+        }
+    }
+    void throwCamera(String camera){
+        this.camera.remove(camera);
+    }
+    void showInfo(){
+
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,16 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-
+        ArrayList<String> camera = new ArrayList<>(Arrays.asList("sony","samsung"));
+        BroadcastTeam gmbs = new BroadcastTeam();
+        gmbs.camera = camera;
+        gmbs.showInfo();
+        System.out.println(gmbs.camera);
+        gmbs.buyCamera("lg");
+        System.out.println(gmbs.camera);
+        gmbs.fixCamera("samsang");
+        System.out.println(gmbs.camera);
     }
 }
